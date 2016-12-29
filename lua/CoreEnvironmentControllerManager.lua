c00a59f55edb8d0193b03a600f3dfec2c79c5fe8
Nox <nox@krondor.fr>
2016-12-29 01:37:34 -0800

    Fix Flashbang crashing the game (Access Violation)

--- lua/CoreEnvironmentControllerManager.lua
+++ lua/CoreEnvironmentControllerManager.lua
@@ -1,7 +1,9 @@
-local set_flashbang_original = CoreEnvironmentControllerManager.set_flashbang
+-- NOTE: Disabled, caused a crash whenever there was a flashbang (ACCESS VIOLATION)
 
-function CoreEnvironmentControllerManager:set_flashbang(...)
-	if not SydneyHUD:GetOption("clean_flashbang") then
-		set_flashbang_original(...)
-	end
-end
+-- local set_flashbang_original = CoreEnvironmentControllerManager.set_flashbang
+--
+-- function CoreEnvironmentControllerManager:set_flashbang(...)
+-- 	if not SydneyHUD:GetOption("clean_flashbang") then
+-- 		set_flashbang_original(...)
+-- 	end
+-- end
