172239a5a555ff75b056901d9ecf47df3b554fab
Katabame Ayame <katabamia@gmail.com>
2017-03-31 08:36:00 +0900

    Add: Auto laser on Closed #12

--- lua/NewRayCastWeaponBase.lua
+++ lua/NewRayCastWeaponBase.lua
@@ -1,4 +1,7 @@
 local setup_original = NewRaycastWeaponBase.setup
+local on_enabled_original = NewRaycastWeaponBase.on_enabled
+local on_equip_original = NewRaycastWeaponBase.on_equip
+local toggle_gadget_original = NewRaycastWeaponBase.toggle_gadget
 
 function NewRaycastWeaponBase:setup(...)
 	setup_original(self, ...)
@@ -10,25 +13,52 @@ function NewRaycastWeaponBase:setup(...)
 	end
 end
 
---[[
-local on_enabled_original = NewRaycastWeaponBase.on_enabled
+function NewRaycastWeaponBase:_setup_laser()
+	if self._has_gadget then
+		local gadgets = clone(self._has_gadget)
+		table.sort(gadgets, function(x, y)
+			local xd = self._parts[x]
+			local yd = self._parts[y]
+			if not xd then
+				return false
+			end
+			if not yd then
+				return true
+			end
+			return xd.unit:base().GADGET_TYPE > yd.unit:base().GADGET_TYPE
+		end)
+
+		for i, part_id in ipairs(gadgets) do
+			local base = self._parts[part_id] and self._parts[part_id].unit:base()
+			if base and base.GADGET_TYPE and base.GADGET_TYPE == (WeaponLaser.GADGET_TYPE or "") then
+				base:set_color_by_theme("player")
+				self:set_gadget_on(i or 0, false)
+				break
+			end
+		end
+	end
+end
 
 function NewRaycastWeaponBase:on_enabled(...)
 	on_enabled_original(self, ...)
 
 	if SydneyHUD:GetOption("auto_laser") and not self._saved_gadget_state then
 		self:_setup_laser()
-
+		--[[
+		if alive(self._second_gun) then
+		self._second_gun:base():_setup_laser()
+		end
+		--]]
 		self._saved_gadget_state = self._gadget_on or 0
 	end
 end
---]]
 
-function NewRaycastWeaponBase:_setup_laser()
-	for _, part in pairs(self._parts) do
-		local base = part.unit and part.unit:base()
-		if base and base.set_color_by_theme then
-			base:set_color_by_theme("player")
-		end
-	end
+function NewRaycastWeaponBase:on_equip(...)
+	on_equip_original(self, ...)
+	self:set_gadget_on(self._saved_gadget_state or 0, false)
+end
+
+function NewRaycastWeaponBase:toggle_gadget(...)
+	toggle_gadget_original(self, ...)
+	self._saved_gadget_state = self._gadget_on or 0
 end
