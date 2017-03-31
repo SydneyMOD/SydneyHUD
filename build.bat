3b713e989d24b2f28e7d354474dde2e4b57ffc8d
Katabame Ayame <katabamia@gmail.com>
2017-03-31 19:07:51 +0900

    Add: french lang

--- build.bat
+++ build.bat
@@ -6,4 +6,4 @@ del SydneyHUD.zip
 echo delete completed.
 
 echo starting compress...
-7za a SydneyHUD.zip ../SydneyHUD/ -xr!.git/ -xr!.vscode/ -xr!.idea/ -xr!installer/ -xr!readme.md -xr!.gitignore -xr!7za.exe -xr!build.bat
+7za a SydneyHUD.zip ../SydneyHUD/ -xr!.git/ -xr!.vscode/ -xr!.idea/ -xr!installer/ -xr!readme.md -xr!.gitignore -xr!7za.exe -xr!build.bat -xr!modworkshop_desc.txt
