00e81f1a681ea5ed29db67ecdaa3c661bac7b3ad
Katabame Ayame <katabamia@gmail.com>
2017-07-07 01:37:40 +0900

    Adjust: travis.yml
    skipping non-project file

--- lua/PlayerCivilian.lua
+++ lua/PlayerCivilian.lua
@@ -4,7 +4,7 @@ local _start_action_interact_original = PlayerCivilian._start_action_interact
 
 function PlayerCivilian:_check_action_interact(t, input, ...)
 	local check_interact = {} and (self._interact_params or 0)
-	end
+
 	if not (self:_check_interact_toggle(t, input) and SydneyHUD:GetOption("push_to_interact") and check_interact >= SydneyHUD:GetOption("push_to_interact_delay")) then
 		return _check_action_interact_original(self, t, input, ...)
 	end
