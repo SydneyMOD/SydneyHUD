df1451b787ba3d558fb8ab364df8402e3410e60f
Katabame Ayame <katabamia@gmail.com>
2017-07-07 01:27:53 +0900

    Remove: anti desync lines

--- lua/HuskPlayerMovement.lua
+++ lua/HuskPlayerMovement.lua
@@ -1,9 +1,3 @@
-local _get_max_move_speed_original = HuskPlayerMovement._get_max_move_speed
-
-function HuskPlayerMovement:_get_max_move_speed(...)
-	return _get_max_move_speed_original(self, ...) * 2 -- the int 2 is const value, TODO: changeable int value
-end
-
 Hooks:PostHook(HuskPlayerMovement, '_start_bleedout', "SydneyHUD:DownOther", function(self, ...)
 	local peer_id = SydneyHUD:Peer_Info(self._unit)
 	if peer_id then
