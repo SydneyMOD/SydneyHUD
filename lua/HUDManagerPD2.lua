fa9e3b4df8af55501edef1e1f0516e9653f88cdb
hexco321 <hexco321@users.noreply.github.com>
2017-07-24 14:34:04 +0100

    Changed press to only show if delay is set to 0
    Needs to get the correct timer value so it only shows press when the conditions are meet

--- lua/HUDManagerPD2.lua
+++ lua/HUDManagerPD2.lua
@@ -280,8 +280,8 @@ function HUDManager.show_interact(self, data)
 	if self._interact_visible and not data.force then
 		return
 	end
-
-	if SydneyHUD:GetOption("push_to_interact") and managers.player:toolset_value() >= SydneyHUD:GetOption("push_to_interact_delay")  then
+	local timer_data = 0 * managers.player:toolset_value()
+	if SydneyHUD:GetOption("push_to_interact") and timer_data >= SydneyHUD:GetOption("push_to_interact_delay")  then
 		data.text = HUDManager:press_substitute(data.text, "Press")
 	end
 
