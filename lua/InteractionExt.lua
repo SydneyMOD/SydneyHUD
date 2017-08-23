2e81ddb0b9f50574778a7b805e87e7860fab9425
kash <kashiwa.electory@gmail.com>
2017-08-23 20:55:08 +0900

    Update: japanese.json Fix: tripmine bugs
    i am going to update my mod, so i may not be able to develop sydneyhud.
    i am sorry for that
    but i make sydneyhud better w/ developers.

--- lua/InteractionExt.lua
+++ lua/InteractionExt.lua
@@ -42,28 +42,33 @@ function BaseInteractionExt:selected(player)
 		selected_original(self, player)
 		local interact_timer = 0
 		if self:_timer_value() then
-		interact_timer = self:_get_timer()
+			interact_timer = self:_get_timer()
 		end
 		if interact_timer < 10 then
 			if string.len(interact_timer) > 3 then
-				interact_timer = string.sub(math.floor(interact_timer * 10 + 0.5) / 10, 1, 3)
+				interact_timer = string.sub(math.round(interact_timer * 10) / 10, 1, 3)
 			elseif string.len(interact_timer) == 1 then
 				interact_timer = interact_timer .. ".0"
 			end
 		else
 			if string.len(interact_timer) > 4 then
-				interact_timer = string.sub(math.floor(interact_timer * 10 + 0.5) / 10, 1, 4)
+				interact_timer = string.sub(math.round(interact_timer * 10) / 10, 1, 4)
 			elseif string.len(interact_timer) == 2 then
 				interact_timer = interact_timer .. ".0"
 			end
 		end
+		local _text_id = self._tweak_data.text_id
 		local _string_macros = {}
 		self:_add_string_macros(_string_macros)
-		local basic_text = managers.localization:text(self._tweak_data.text_id, _string_macros)
-		managers.hud:show_interact({
-			text = basic_text .. " (" .. interact_timer .. " s)",
-			icon = self._tweak_data.icon
-		})
+		if _text_id then
+			local basic_text = managers.localization:text(_text_id, _string_macros)
+			managers.hud:show_interact({
+				text = basic_text .. " (" .. interact_timer .. " s)",
+				icon = self._tweak_data.icon
+			})
+		else
+			selected_original(self, player)
+		end
 	end
 	return true
 end
