365beaad085dc335ba939b7f70973daf6fc79e03
kash <kashiwa.electory@gmail.com>
2017-09-02 21:20:30 +0900

    Update: InteractionExt.lua

--- lua/InteractionExt.lua
+++ lua/InteractionExt.lua
@@ -22,11 +22,7 @@ function SecurityCameraInteractionExt:set_active(active, ...)
 end
 
 function BaseInteractionExt:interact_start(player, data)
-	local interact_timer = 0
-	if self:_timer_value() then
-		interact_timer = self:_get_timer()
-	end
-	if SydneyHUD:GetOption("push_to_interact") and self:can_interact(player) and interact_timer >= SydneyHUD:GetOption("push_to_interact_delay") then
+	if SydneyHUD:GetOption("push_to_interact") and self:can_interact(player) and tonumber(self:check_interact_time()) >= SydneyHUD:GetOption("push_to_interact_delay") then
 		local btn_cancel = SydneyHUD:GetOption("equipment_interrupt") and (managers.localization:btn_macro("use_item", true) or managers.localization:get_default_macro("BTN_USE_ITEM")) or (managers.localization:btn_macro("interact", true) or managers.localization:get_default_macro("BTN_INTERACT"))
 		managers.hud:show_interact({
 			text = managers.localization:text(self.tweak_data == "corpse_alarm_pager" and "sydneyhud_int_locked_pager" or "sydneyhud_int_locked", {BTN_CANCEL = btn_cancel}),
@@ -39,36 +35,47 @@ end
 
 function BaseInteractionExt:selected(player)
 	if SydneyHUD:GetOption("interact_time_hint") then
-		selected_original(self, player)
-		local interact_timer = 0
-		if self:_timer_value() then
-			interact_timer = self:_get_timer()
-		end
-		if interact_timer < 10 then
-			if string.len(interact_timer) > 3 then
-				interact_timer = string.sub(math.round(interact_timer * 10) / 10, 1, 3)
-			elseif string.len(interact_timer) == 1 then
-				interact_timer = interact_timer .. ".0"
-			end
-		else
-			if string.len(interact_timer) > 4 then
-				interact_timer = string.sub(math.round(interact_timer * 10) / 10, 1, 4)
-			elseif string.len(interact_timer) == 2 then
-				interact_timer = interact_timer .. ".0"
-			end
-		end
 		local _text_id = self._tweak_data.text_id
 		local _string_macros = {}
 		self:_add_string_macros(_string_macros)
 		if _text_id then
-			local basic_text = managers.localization:text(_text_id, _string_macros)
-			managers.hud:show_interact({
-				text = basic_text .. " (" .. interact_timer .. " s)",
-				icon = self._tweak_data.icon
-			})
+			if self.tweak_data ~= TripMineBase:get_name_id() then
+				local basic_text = managers.localization:text(_text_id, _string_macros)
+				managers.hud:show_interact({
+					text = basic_text .. " (" .. self:check_interact_time() .. " s)",
+					icon = self._tweak_data.icon
+				})
+			else
+				local basic_text = managers.localization:text(TripMineBase:interaction_text_id(), _string_macros)
+				managers.hud:show_interact({
+					text = basic_text .. " (0.0 s)",
+					icon = "equipment_trip_mine"
+				})
+			end
 		else
-			selected_original(self, player)
+			selected_original(self, player) 
 		end
 	end
 	return true
+end
+
+function BaseInteractionExt:check_interact_time()
+	local interact_timer = 0
+	if self:_timer_value() then
+		interact_timer = self:_get_timer()
+	end
+	if interact_timer < 10 then
+		if string.len(interact_timer) > 3 then
+			interact_timer = string.sub(math.round(interact_timer * 10) / 10, 1, 3)
+		elseif string.len(interact_timer) == 1 then
+			interact_timer = interact_timer .. ".0"
+		end
+	else
+		if string.len(interact_timer) > 4 then
+			interact_timer = string.sub(math.round(interact_timer * 10) / 10, 1, 4)
+		elseif string.len(interact_timer) == 2 then
+			interact_timer = interact_timer .. ".0"
+		end
+	end
+	return interact_timer
 end
