4c628c02cda2de1596fd5d8cae409946793a9786
Katabame Ayame <katabamia@gmail.com>
2017-10-11 18:39:43 +0900

    Delete: item slider tweaks

--- lua/CoreItemSlider.lua
+++
@@ -1,7 +0,0 @@
-core:module("CoreMenuItemSlider")
-local set_value_original = ItemSlider.set_value
-
-function ItemSlider:set_value(value)
-    value = self._min + self._step * math.round((value - self._min) / self._step)
-    set_value_original(self, value)
-end
