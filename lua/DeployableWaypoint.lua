4495a08fd9159d5516a71241309730048116b01d
hexco321 <hexco321@users.noreply.github.com>
2017-10-16 13:54:43 +0100

    Added underdog indicator, timer and deployable waypoints

---
+++ lua/DeployableWaypoint.lua
@@ -0,0 +1,169 @@
+if not SydneyHUD:GetOption("show_deployable_waypoint") then return end
+
+DeployableWaypoint = DeployableWaypoint or class(CustomWaypoint)
+function DeployableWaypoint:init(id, ws, data)
+	DeployableWaypoint.super.init(self, id, ws, data)
+	
+	self._type = data.type
+	self._panel:set_size(35, 60)
+	
+	self._icon = self._panel:bitmap({
+		name = "icon",
+		texture = data.texture,
+		texture_rect = data.texture_rect,
+		w = self._panel:w(),
+		h = self._panel:w(),
+	})
+	
+	local text_size = self._panel:h() - self._icon:h()
+	self._text = self._panel:text({
+		name = "text",
+		font = tweak_data.hud.medium_font_noshadow,
+		font_size = text_size * 0.95,
+		align = "center",
+		vertical = "center",
+		w = self._panel:w(),
+		h = text_size,
+		y = self._icon:h(),
+	})
+	
+	self._cached_data = data.deployable_data or {}
+end
+
+function DeployableWaypoint:post_init()
+	DeployableWaypoint.super.post_init(self)
+	
+	self:set_amount(self._cached_data)
+	self:set_owner(self._cached_data)
+	self:set_upgrades(self._cached_data)
+	
+	self._cached_data = nil
+end
+
+function DeployableWaypoint:_check_onscreen_visibility(distance, ...)
+	return distance < 1000
+end
+
+function DeployableWaypoint:set_amount(data)
+
+end
+
+function DeployableWaypoint:set_owner(data)
+	local color = data.owner and tweak_data.chat_colors[data.owner] and tweak_data.chat_colors[data.owner]:with_alpha(1) or Color.white
+	self._icon:set_color(color)
+end
+
+function DeployableWaypoint:set_upgrades(data)
+
+end
+
+
+BagDeployableWaypoint = BagDeployableWaypoint or class(DeployableWaypoint)
+function BagDeployableWaypoint:set_amount(data)
+	local amount = (data.amount or 0) + (data.amount_offset or 0)
+	self._text:set_text(string.format("%.0f", amount))
+end
+
+
+AmmoBagDeployableWaypoint = AmmoBagDeployableWaypoint or class(BagDeployableWaypoint)
+function AmmoBagDeployableWaypoint:init(...)
+	AmmoBagDeployableWaypoint.super.init(self, ...)
+	
+	self._panel:set_h(self._panel:h() + self._icon:h())
+	
+	local size = self._icon:h()
+	
+	self._upgrade_icon = self._panel:bitmap({
+		name = "upgrade_icon",
+		texture = "guis/textures/pd2/skilltree_2/icons_atlas_2",
+		texture_rect = { 4 * 80, 5 * 80, 80, 80 },
+		w = size,
+		h = size,
+		visible = false,
+	})
+	self._upgrade_icon:set_bottom(self._panel:h())
+	
+	self._aced_icon = self._panel:bitmap({
+		name = "aced_icon",
+		texture = "guis/textures/pd2/skilltree_2/ace_symbol",
+		w = self._upgrade_icon:w(),
+		h = self._upgrade_icon:w(),
+		visible = false,
+		layer = self._upgrade_icon:layer() - 1,
+	})
+	self._aced_icon:set_center(self._upgrade_icon:center())
+end
+
+function AmmoBagDeployableWaypoint:set_amount(data)
+	local amount = (data.amount or 0) + (data.amount_offset or 0)
+	self._text:set_text(string.format("%.0f%%", amount * 100))
+end
+
+function AmmoBagDeployableWaypoint:set_upgrades(data)
+	local bullet_storm_level = data.upgrades and data.upgrades.bullet_storm or 0
+	self._upgrade_icon:set_visible(bullet_storm_level > 0)
+	self._aced_icon:set_visible(bullet_storm_level > 1)
+end
+
+
+SentryDeployableWaypoint = SentryDeployableWaypoint or class(DeployableWaypoint)
+function SentryDeployableWaypoint:set_amount(data)
+	local amount = data.ammo_ratio or 0
+	self._text:set_text(string.format("%.0f%%", amount * 100))
+end
+
+
+
+local wp_map = {
+	ammo_bag = { texture = "guis/textures/pd2/skilltree/icons_atlas", texture_rect = { 1*64, 0, 64, 64 }, class = AmmoBagDeployableWaypoint },
+	doc_bag = { texture = "guis/textures/pd2/skilltree/icons_atlas", texture_rect = { 2*64, 7*64, 64, 64 }, class = BagDeployableWaypoint },
+	body_bag = { texture = "guis/textures/pd2/skilltree/icons_atlas", texture_rect = { 5*64, 11*64, 64, 64 }, class = BagDeployableWaypoint },
+	grenade_crate = { texture = "guis/dlcs/big_bank/textures/pd2/pre_planning/preplan_icon_types", texture_rect = { 1*48, 0, 48, 48 }, class = BagDeployableWaypoint },
+	sentry = { texture = "guis/textures/pd2/skilltree/icons_atlas", texture_rect = { 7*64, 5*64, 64, 64 }, class = SentryDeployableWaypoint },
+}
+
+local function add_wp(id, type, unit, key)
+	local map = wp_map[type]
+	local class = map.class or DeployableWaypoint
+	local params = {
+		position = unit:interaction():interact_position(),
+		type = type,
+		texture = map.texture,
+		texture_rect = map.texture_rect,
+		deployable_data = managers.gameinfo:get_deployables(type, key),
+	}
+	
+	managers.waypoints:add_waypoint(id, class, params)
+end
+
+local function bag_clbk(event, key, data)
+	if data.aggregate_members then return end
+	
+	local id = "bag_wp_" .. key
+	
+	if event == "set_active" then
+		if data.active then
+			add_wp(id, data.type, data.unit, key)
+		else
+			managers.waypoints:remove_waypoint(id)
+		end
+	elseif managers.waypoints:get_waypoint(id) then
+		if event == "set_amount_offset" then event = "set_amount" end
+		
+		managers.waypoints:do_callback(id, event, data)
+	end
+end
+
+local function add_events()
+	for _, t in pairs({ "ammo_bag", "doc_bag", "body_bag", "grenade_crate" }) do
+		managers.gameinfo:register_listener(t .. "_waypoint_listener", t, "set_active", bag_clbk)
+		managers.gameinfo:register_listener(t .. "_waypoint_listener", t, "set_owner", bag_clbk)
+		managers.gameinfo:register_listener(t .. "_waypoint_listener", t, "set_amount", bag_clbk)
+		managers.gameinfo:register_listener(t .. "_waypoint_listener", t, "set_amount_offset", bag_clbk)
+		managers.gameinfo:register_listener(t .. "_waypoint_listener", t, "set_upgrades", bag_clbk)
+	end
+end
+
+if GameInfoManager then
+	GameInfoManager.add_post_init_event(add_events)
+end
---
+++ lua/TimerWaypoint.lua
@@ -0,0 +1,141 @@
+if not SydneyHUD:GetOption("show_timer_waypoint") then return end
+
+TimerWaypoint = TimerWaypoint or class(CustomWaypoint)
+	
+local function format_time_string(value)
+	local frmt_string
+
+	if value >= 60 then
+		frmt_string = string.format("%d:%02d", math.floor(value / 60), math.ceil(value % 60))
+	elseif value >= 9.9 then
+		frmt_string = string.format("%d", math.ceil(value))
+	elseif value >= 0 then
+		frmt_string = string.format("%.1f", value)
+	else
+		frmt_string = string.format("%.1f", 0)
+	end
+	
+	return frmt_string
+end	
+	
+function TimerWaypoint:init(id, ws, data)
+	TimerWaypoint.super.init(self, id, ws, data)
+	
+	self._panel:set_size(40, 60)
+	
+	self._icon = self._panel:bitmap({
+		name = "icon",
+		texture = data.texture,
+		texture_rect = data.texture_rect,
+		w = self._panel:w() * 0.875,
+		h = self._panel:w() * 0.875,
+		x = self._panel:w() * 0.125,
+	})
+	
+	local text_size = self._panel:h() - self._icon:h()
+	self._text = self._panel:text({
+		name = "text",
+		font = tweak_data.hud.medium_font_noshadow,
+		font_size = text_size * 0.95,
+		align = "center",
+		vertical = "center",
+		w = self._panel:w(),
+		h = text_size,
+		y = self._icon:h(),
+	})
+end
+
+function TimerWaypoint:_check_offscreen_visibility(...)
+	return self._disabled and TimerWaypoint.super._check_offscreen_visibility(self, ...)
+end
+
+function TimerWaypoint:_onscreen_state_change()
+	self._text:set_visible(self._onscreen)
+	
+	if self._onscreen then
+		self._icon:set_y(0)
+	else
+		self._arrow_radius = self._icon:w() * 0.75
+		self._icon:set_center(self._panel:w()/2, self._panel:h() / 2)
+	end
+	
+	return TimerWaypoint.super._onscreen_state_change(self)
+end
+
+function TimerWaypoint:set_text(value)
+	--self._text:set_text(string.format("%.0f", value))
+	self._text:set_text(format_time_string(value))
+end
+
+function TimerWaypoint:set_jammed(status)
+	self._jammed = status
+	self:_check_running()
+end
+
+function TimerWaypoint:set_powered(status)
+	self._unpowered = not status
+	self:_check_running()
+end
+
+function TimerWaypoint:_check_running()
+	self._disabled = self._jammed or self._unpowered
+	local color = self._disabled and Color.red or Color.white
+	self._icon:set_color(color)
+	self._text:set_color(color)
+	self._arrow:set_color(color)
+end
+
+
+MeltdownTimerWaypoint = MeltdownTimerWaypoint or class(TimerWaypoint)
+
+function MeltdownTimerWaypoint:set_text(value)
+	self._text:set_text(string.format("%d/50", math.floor(value)))
+end
+
+
+local icon_table = {
+	drill = "pd2_drill",
+	hack = "pd2_computer",
+	saw = "wp_saw",
+	timer = "pd2_computer",
+	securitylock = "pd2_computer",
+	digital = "pd2_computer",
+}
+
+local function timer_clbk(event, key, data)
+	local id = "timer_wp_" .. key
+
+	if event == "set_active" then
+		if data.active then
+			local texture, texture_rect = tweak_data.hud_icons:get_icon_data(icon_table[data.device_type or "timer"])
+			local params = {
+				position = data.unit:position(),
+				texture = texture,
+				texture_rect = texture_rect,
+				show_offscreen = true,
+			}
+			
+			managers.waypoints:add_waypoint(id, data.id == 132864 and MeltdownTimerWaypoint or TimerWaypoint, params)
+		else
+			managers.waypoints:remove_waypoint(id)
+		end
+	elseif event == "update" then
+		managers.waypoints:do_callback(id, "set_text", (data.timer_value or 0))
+	elseif event == "set_jammed" then
+		managers.waypoints:do_callback(id, "set_jammed", data.jammed)
+	elseif event == "set_powered" then
+		managers.waypoints:do_callback(id, "set_powered", data.powered)
+	end
+end
+
+
+local function add_events()
+	managers.gameinfo:register_listener("timer_waypoint_listener", "timer", "set_active", timer_clbk)
+	managers.gameinfo:register_listener("timer_waypoint_listener", "timer", "update", timer_clbk)
+	managers.gameinfo:register_listener("timer_waypoint_listener", "timer", "set_jammed", timer_clbk)
+	managers.gameinfo:register_listener("timer_waypoint_listener", "timer", "set_powered", timer_clbk)
+end
+
+if GameInfoManager then
+	GameInfoManager.add_post_init_event(add_events)
+end
